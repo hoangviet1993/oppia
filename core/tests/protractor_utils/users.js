f730c497ec3ca807382d51a694d7198110e3c122
hoangviet1993 <hoangviet1993@gmail.com>
2018-07-18 23:50:50 -0400

    users: Reverting changes to login

--- core/tests/protractor_utils/users.js
+++ core/tests/protractor_utils/users.js
@@ -24,12 +24,12 @@ var waitFor = require('./waitFor.js');
 var AdminPage = require('../protractor_utils/AdminPage.js');
 var adminPage = new AdminPage.AdminPage();
 
-var loginButton = element(by.css('[ng-if="!username"]'));
 var driver = browser.driver;
+var loginButton = element(by.css('[ng-if="!username"]'));
+var loginUrl = general.SERVER_URL_PREFIX + general.LOGIN_URL_SUFFIX;
 
 var loginPageLoaded = function() {
   // Waiting for url to change.
-  var loginUrl = 'http://localhost:9001/_ah/login?continue=http%3A//localhost%3A9001/signup%3Freturn_url%3D%252F';
   return driver.getCurrentUrl().then(function(currentUrl) {
     return currentUrl === loginUrl;
   });
@@ -37,13 +37,9 @@ var loginPageLoaded = function() {
 
 var login = function(email, isSuperAdmin) {
   // User need to be logged out before logging in a new account.
-  browser.get(general.SERVER_URL_PREFIX);
-  waitFor.pageToFullyLoad();
-  waitFor.elementToBeClickable(loginButton, 'Login button is not clickable');
-  loginButton.click();
-
   // Use of element is not possible because the login page is non-angular.
   // The full url is also necessary.
+  driver.get(loginUrl);
   browser.wait(loginPageLoaded, 10000, 'Login page takes too long to be ready');
   driver.findElement(protractor.By.name('email')).clear();
   driver.findElement(protractor.By.name('email')).sendKeys(email);
@@ -51,13 +47,13 @@ var login = function(email, isSuperAdmin) {
     driver.findElement(protractor.By.name('admin')).click();
   }
   driver.findElement(protractor.By.id('submit-login')).click();
-  waitFor.pageToFullyLoad();
 };
 
 var logout = function() {
   browser.get(general.SERVER_URL_PREFIX);
   waitFor.pageToFullyLoad();
   var profileDropdown = element(by.css('.protractor-test-profile-dropdown'));
+  waitFor.elementToBeClickable(profileDropdown, 'Profile is not clickable');
   profileDropdown.click();
   var logoutButton = element(by.css('[ng-href="/logout?return_url=%2F"]'));
   waitFor.elementToBeClickable(logoutButton, 'Logout button is not clickable');
